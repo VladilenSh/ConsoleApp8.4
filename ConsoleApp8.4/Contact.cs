b8d1fe07d818a7aeb9c59a89cde48c45a64a0cfa
VladilenSh <springs@yandex.ru>
2022-05-24 18:57:46 +0700

    +
    +

---
+++ ConsoleApp8.4/Contact.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8._4
+{
+    class Contact
+    {
+        #region Поля
+        private string fullName;
+
+        private string street;
+
+        private string houseNumber;
+
+        private int flatNumber;
+
+        private string mobilePhone;
+
+        private string flatPhone;
+
+        #endregion
+
+        #region Свойства
+        public string FullName { get { return this.fullName; } set { this.fullName = value; } }
+
+        public string Street { get { return this.street; } set { this.street = value; } }
+
+        public string HouseNumber { get { return this.houseNumber; } set { this.houseNumber = value; } }
+
+        public int FlatNumber { get { return this.flatNumber; } set { this.flatNumber = value; } }
+
+        public string MobilePhone { get { return this.mobilePhone; } set { this.mobilePhone = value; } }
+
+        public string FlatPhone { get { return this.flatPhone; } set { this.flatPhone = value; } }
+
+        #endregion
+
+        #region Конструкторы
+        public Contact (string FullName, string Street, string HouseNumber, int FlatNumber, string MobilePhone, string FlatPhone)
+        {
+            this.fullName = FullName;
+            this.street = Street;
+            this.houseNumber = HouseNumber;
+            this.flatNumber = FlatNumber;
+            this.mobilePhone = MobilePhone;
+            this.flatPhone = FlatPhone;
+        }
+        #endregion
+    }
+}
---
+++ ConsoleApp8.4/Program.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace ConsoleApp8._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = "data.json";
+
+            string fullName = "";
+            string street = "";
+            string houseNumber = "";
+            int flatNumber = 0;
+            string mobilePhone = "";
+            string flatPhone = "";
+
+            Console.WriteLine("фио");
+            fullName = Console.ReadLine();
+            Console.WriteLine("Улица");
+            street = Console.ReadLine();
+            Console.WriteLine("Номер дома");
+            houseNumber = Console.ReadLine();
+            Console.WriteLine("Номер квартиры");
+            int.TryParse(Console.ReadLine(),out flatNumber);
+            Console.WriteLine("Мобильный телефон");
+            mobilePhone = Console.ReadLine();
+            Console.WriteLine("Домашний телефон");
+            flatPhone = Console.ReadLine();
+
+            Contact contact = new Contact(fullName,street,houseNumber,flatNumber,mobilePhone,flatPhone);
+
+            string json1 = JsonConvert.SerializeObject(contact);
+
+            File.WriteAllText(fileName, json1);
+        }
+    }
+}
